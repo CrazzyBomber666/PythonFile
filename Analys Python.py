115792d0ea9b42381bec66d5c9a6aec1c309a919
CrazzyBomber666 <assassin0912@mail.ru>
2022-05-10 01:20:30 +0300

    симуляция жизни мужа и жены (есть повторяющий код)

--- Analys Python.py
+++ Analys Python.py
@@ -235,3 +235,267 @@ elif response.status_code == 404:
 Сайты не будут идеть, что это автоматизированное средство для тестирования
 """
 
+# Создать модель жизни небольшой семьи.
+#
+# Каждый день участники жизни могут делать только одно действие.
+# Все вместе они должны прожить год и не умереть.
+#
+# Муж может:
+#   есть,
+#   играть в WoT,
+#   ходить на работу,
+# Жена может:
+#   есть,
+#   покупать продукты,
+#   покупать шубу,
+#   убираться в доме,
+ 
+# Все они живут в одном доме, дом характеризуется:
+#   кол-во денег в тумбочке (в начале - 100)
+#   кол-во еды в холодильнике (в начале - 50)
+#   кол-во грязи (в начале - 0)
+#
+# У людей есть имя, степень сытости (в начале - 30) и степень счастья (в начале - 100).
+#
+# Любое действие, кроме "есть", приводит к уменьшению степени сытости на 10 пунктов
+# Кушают взрослые максимум по 30 единиц еды, степень сытости растет на 1 пункт за 1 пункт еды.
+# Степень сытости не должна падать ниже 0, иначе чел умрет от голода.
+#
+# Деньги в тумбочку добавляет муж, после работы - 150 единиц за раз.
+# Еда стоит 10 денег 10 единиц еды. Шуба стоит 350 единиц.
+#
+# Грязь добавляется каждый день по 5 пунктов, за одну уборку жена может убирать до 100 единиц грязи.
+# Если в доме грязи больше 90 - у людей падает степень счастья каждый день на 10 пунктов,
+# Степень счастья растет: у мужа от игры в WoT (на 20), у жены от покупки шубы (на 60, но шуба дорогая)
+# Степень счастья не должна падать ниже 10, иначе чел умрает от депресии.
+#
+# Подвести итоги жизни за год: сколько было заработано денег, сколько сьедено еды, сколько куплено шуб.
+
+from random import randint
+
+class House:
+
+    def __init__(self):
+        self.__money = 100
+        self.__food = 50
+        self.__ashed = 0
+
+    def _get_food(self):
+        return self.__food
+
+    def _get_money(self):
+        return self.__money
+
+    def _get_ashed(self):
+        return self.__ashed
+
+    def _set_food(self, count_food):
+        self.__food += count_food
+
+    def _set_money_after_buy(self, price):
+        self.__money -= price
+
+    def _set_money_after_work(self, price):
+        self.__money += price
+
+    def set_ashed(self):
+        self.__ashed += 5
+
+    def _set_ashed(self):
+        self.__ashed = 0
+
+    def __str__(self):
+        return 'В доме денег: {}, еды: {}, грязи: {}'.format(*self.__dict__.values())
+
+class Human:
+
+    def __init__(self, house):
+        self.__fullness = 30
+        self.__happiness = 100
+        self.__house = house
+
+    def _get_fullness(self):
+        return self.__fullness
+
+    def _get_happiness(self):
+        return self.__happiness
+
+    def _get_house_food(self):
+        return self.__house._get_food()
+
+    def _get_house_money(self):
+        return self.__house._get_money()
+
+    def _get_house_ashed(self):
+        return self.__house._get_ashed()
+
+    def _set_house_ashed(self, name):
+        self.__house._set_ashed()
+        print('{} убрала квартиру'.format(name))
+
+    def _set_house(self):
+        self.__house._set_money_after_work(price=150)
+
+    def _set_fullness_after_eat(self, name, fullness):
+        if self.__fullness + fullness > 100:
+            self.__fullness = 100
+        else:
+            self.__fullness += fullness
+            self.__house._set_food(count_food=-fullness)
+        print('{} покушал(а)'.format(name))
+
+    def _set_fullness_after_other_act(self, fullness):
+        self.__fullness -= fullness
+
+    def _set_happiness(self, count_happiness):
+        if self.__happiness + count_happiness > 100:
+            self.__happiness = 100
+        else:
+            self.__happiness += count_happiness
+
+    def _buy_products(self, name, price):
+        if self._get_house_money() - price > 0:
+            self.__house._set_money_after_buy(price)
+            self.__house._set_food(price)
+            print('{} сходила в магазин и купила продукты на сумму {}'.format(name, price))
+            return True
+        else:
+            return False
+
+    def __str__(self, name):
+        return 'У {} степень сытости: {}, степень счастья: {}'.format(name, *self.__dict__.values())
+
+class Husband(Human):
+
+    def __init__(self, name, house):
+        super().__init__(house)
+        self.__name = name
+
+    def __str__(self):
+        return super().__str__(name=self.__name)
+
+    def eat(self):
+        fullness = randint(1, 30)
+        if super()._get_house_food() - fullness < 0:
+            print('{} не хватает еды'.format(self.__name))
+            super()._set_happiness(count_happiness=-2)
+            super()._set_fullness_after_other_act(fullness=10)
+        else:
+            super()._set_fullness_after_eat(name=self.__name, fullness=fullness)
+            super()._set_happiness(count_happiness=-2)
+
+    def play_game_WoT(self):
+        super()._set_fullness_after_other_act(fullness=10)
+        super()._set_happiness(count_happiness=20)
+        print('{} поиграл в World Of Tanks'.format(self.__name))
+
+    def go_work(self):
+        super()._set_fullness_after_other_act(fullness=10)
+        super()._set_house()
+        super()._set_happiness(count_happiness=-10)
+        print('{} сходил на работу и заработал 150 денег'.format(self.__name))
+
+    def act(self):
+        if super()._get_fullness() <= 0 or super()._get_happiness() <= 0:
+            print('--'*10 + '{} умер!!!!'.format(self.__name))
+        elif super()._get_house_money() < 60:
+            self.go_work()
+        elif super()._get_fullness() <= 30:
+            self.eat()
+        elif super()._get_happiness() <= 30:
+            self.play_game_WoT()
+        elif super()._get_house_money() <= 60:
+            self.go_work()
+        else:
+            act = randint(1, 3)
+            match act:
+                case 1:
+                    self.play_game_WoT()
+                case 2:
+                    self.eat()
+                case 3:
+                    self.go_work()
+
+class Wife(Human):
+
+    def __init__(self, name, house):
+        super().__init__(house)
+        self.__name = name
+
+    def __str__(self):
+        return super().__str__(name=self.__name)
+        
+    def eat(self):
+        fullness = randint(1, 30)
+        if super()._get_house_food() - fullness < 0:
+            print('{} не хватает еды'.format(self.__name))
+            super()._set_happiness(count_happiness=-2)
+            super()._set_fullness_after_other_act(fullness=10)
+        else:
+            super()._set_fullness_after_eat(name=self.__name, fullness=fullness)
+            super()._set_happiness(count_happiness=-2)
+
+    def buy_products(self):
+        if super()._buy_products(name=self.__name, price=randint(30, 60)):
+            super()._set_happiness(count_happiness=-10)
+            super()._set_fullness_after_other_act(fullness=10)
+        else:
+            print('{} не хватило денег на продукты'.format(self.__name))
+            super()._set_fullness_after_other_act(fullness=10)
+            super()._set_happiness(count_happiness=-10)
+
+    def buy_shuba(self):
+        if super()._buy_products(name=self.__name, price=150):
+            super()._set_happiness(count_happiness=60)
+            super()._set_fullness_after_other_act(fullness=10)
+        else:
+            print('{} не хватило денег на шубу'.format(self.__name))
+            super()._set_happiness(count_happiness=-10)
+            super()._set_fullness_after_other_act(fullness=10)
+
+    def do_out_in_home(self):
+        super()._set_house_ashed(name=self.__name)
+        super()._set_fullness_after_other_act(fullness=10)
+        super()._set_happiness(count_happiness=-20)
+        
+    def act(self):
+        if super()._get_fullness() <= 0 or super()._get_happiness() <= 0:
+            print('--'*10 + '{} умер!!!!'.format(self.__name))
+        elif super()._get_house_food() <= 60:
+            self.buy_products()
+        elif super()._get_happiness() <= 30:
+            self.buy_shuba()
+        elif super()._get_fullness() <= 30:
+            self.eat()
+        elif super()._get_house_ashed() >= 80:
+            self.do_out_in_home()
+        else:
+            act = randint(1, 3)
+            match act:
+                case 1:
+                    self.buy_products()
+                case 2:
+                    self.buy_shuba()
+                case 3:
+                    self.do_out_in_home()
+            pass
+
+if __name__ == '__main__':
+    home = House()
+    maks = Husband(name='Макс', house=home)
+    masha = Wife(name='Маша', house=home)
+    print()
+    print(maks)
+    print(masha)
+    print(home)
+
+    year = 30
+    for day in range(1, 365 * year + 1):
+        print('-'*25 + str(day) + ' День' + str('-')*25)
+        maks.act()
+        masha.act()
+        home.set_ashed()
+        print(maks)
+        print(masha)
+        print(home)
+    
